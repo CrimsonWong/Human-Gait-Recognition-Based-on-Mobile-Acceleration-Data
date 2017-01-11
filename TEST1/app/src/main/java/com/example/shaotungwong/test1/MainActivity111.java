d16e441caa97c3b37f85b412bda7f0e33c1f7d9e
CrimsonWong <crimsonwong@foxmail.com>
2017-01-11 14:27:04 +0800

    Test
    First test of HELLO WORLD in Android studio

---
+++ TEST1/app/src/main/java/com/example/shaotungwong/test1/MainActivity111.java
@@ -0,0 +1,13 @@
+package com.example.shaotungwong.test1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity111 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main111);
+    }
+}
